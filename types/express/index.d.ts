a05962401ae60b33bbcb09cc5b851aa1302b77a4
AlbaSly <arias.brando.raxel@gmail.com>
2022-03-18 11:26:11 -0600

    Add IUserRequest as a valid object in express Request

---
+++ types/express/index.d.ts
@@ -0,0 +1,11 @@
+import {Request} from 'express';
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        user: IUserRequest
+    }
+}
+
+interface IUserRequest {
+    id: string
+}
